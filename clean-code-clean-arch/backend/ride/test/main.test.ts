e615a897ed2bff74b4cd0ef024dc13f004f8f37d
Ibrahim <ibrahim.borba@gmail.com>
2023-11-29 01:37:22 -0300

    test: add testes for calculate ride

---
+++ clean-code-clean-arch/backend/ride/test/main.test.ts
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+test('Deve fazer o calculo de preco de uma corrida durante o dia', async function () {
+    const input = [
+        { dist: 10, ds: '2021-03-01T10:00:00'}
+    ];
+    const response = await axios.post("http://localhost:3000/calc", input)
+    const output = response.data;
+    expect(output.result).toBe(21);
+});
+
+test('Deve fazer o calculo de preco de uma corrida durante a noite', async function () {
+    const input = [
+        { dist: 10, ds: '2021-03-01T23:00:00'}
+    ];
+    const response = await axios.post("http://localhost:3000/calc", input)
+    const output = response.data;
+    expect(output.result).toBe(39);
+});
+
+test('Deve fazer o calculo de preco de uma corrida no domingo de dia', async function () {
+    const input = [
+        { dist: 10, ds: '2021-03-07T10:00:00'}
+    ];
+    const response = await axios.post("http://localhost:3000/calc", input)
+    const output = response.data;
+    expect(output.result).toBe(29);
+});
+
+test('Deve fazer o calculo de preco de uma corrida no domingo de noite', async function () {
+    const input = [
+        { dist: 10, ds: '2021-03-07T23:00:00'}
+    ];
+    const response = await axios.post("http://localhost:3000/calc", input)
+    const output = response.data;
+    expect(output.result).toBe(50);
+});
+
+test('Deve retornar -1 se a distancia for invalida', async function () {
+    const input = [
+        { dist: -10, ds: '2021-03-07T23:00:00'}
+    ];
+    const response = await axios.post("http://localhost:3000/calc", input)
+    const output = response.data;
+    expect(output.result).toBe(-1);
+});
+
+test('Deve retornar -2 se a data for invalida', async function () {
+    const input = [
+        { dist: 10, ds: 'data'}
+    ];
+    const response = await axios.post("http://localhost:3000/calc", input)
+    const output = response.data;
+    expect(output.result).toBe(-2);
+});
