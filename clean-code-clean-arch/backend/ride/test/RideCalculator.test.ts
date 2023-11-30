f0c02823b34f249bea99a51cc95228cf5230aa2c
Ibrahim <ibrahim.borba@gmail.com>
2023-11-30 01:22:47 -0300

    feat: rename main to api and implements class for Ride

--- clean-code-clean-arch/backend/ride/test/RideCalculator.test.ts
+++ clean-code-clean-arch/backend/ride/test/RideCalculator.test.ts
@@ -1,11 +1,67 @@
-import { isOvernight, isSunday } from "../src/RideCalculator"
+import axios from 'axios';
+import { calculate } from '../src/RideCalculator';
 
-test('Deve testar se é de noite', function name() {
-    const segment = { distance: 10, date: new Date('2021-03-07T23:00:00')}
-    expect(isOvernight(segment)).toBeTruthy();
-})
+test('Deve fazer o calculo de preco de uma corrida durante o dia', function () {
+    const segments = [
+        { distance: 10, date: '2021-03-01T10:00:00'}
+    ];
+    const price = calculate(segments);
+    expect(price).toBe(21);
+});
 
-test('Deve testar se é domingo', function name() {
-    const segment = { distance: 10, date: new Date('2021-03-07T10:00:00')}
-    expect(isSunday(segment)).toBeTruthy();
-})
+test('Deve fazer o calculo de preco de uma corrida durante a noite', function () {
+    const segments = [
+        { distance: 10, date: '2021-03-01T23:00:00'}
+    ];
+    const price = calculate(segments);
+    expect(price).toBe(39);
+});
+
+test('Deve fazer o calculo de preco de uma corrida no domingo de dia', function () {
+    const segments = [
+        { distance: 10, date: '2021-03-07T10:00:00'}
+    ];
+    const price = calculate(segments);
+    expect(price).toBe(29);
+});
+
+test('Deve fazer o calculo de preco de uma corrida no domingo de noite', function () {
+    const segments = [
+        { distance: 10, date: '2021-03-07T23:00:00'}
+    ];
+    const price = calculate(segments);
+    expect(price).toBe(50);
+});
+
+test('Deve retornar -1 se a distancia for invalida', function () {
+    const segments = [
+        { distance: -10, date: '2021-03-07T23:00:00'}
+    ];
+    const price = calculate(segments);
+    expect(price).toBe(-1);
+});
+
+test('Deve retornar -2 se a data for invalida', function () {
+    const segments = [
+        { distance: 10, date: 'data'}
+    ];
+    const price = calculate(segments);
+    expect(price).toBe(-2);
+});
+
+test('Deve ter o preco minimo de 10', function () {
+    const segments = [
+        { distance: 1, date: '2021-03-01T10:00:00'}
+    ];
+    const price = calculate(segments);
+    expect(price).toBe(10);
+});
+
+test('Deve fazer o calculo de preco de uma corrida com multiplos segmentos', function () {
+    const segments = [
+        { distance: 10, date: '2021-03-01T10:00:00'},
+        { distance: 10, date: '2021-03-01T12:00:00'}
+    ];
+    const price = calculate(segments);
+    expect(price).toBe(42);
+});
