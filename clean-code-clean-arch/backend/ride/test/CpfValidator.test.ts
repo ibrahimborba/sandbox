22c4c289bc475c1ea39ef7228f59a6541ccbc848
Ibrahim <ibrahim.borba@gmail.com>
2023-12-01 01:36:35 -0300

    test: refactor test for cpf validation using class

--- clean-code-clean-arch/backend/ride/test/CpfValidator.test.ts
+++ clean-code-clean-arch/backend/ride/test/CpfValidator.test.ts
@@ -1,12 +1,12 @@
-import { validate } from "../src/cpf"
+import Cpf from "../src/Cpf";
 
 test.each([
     "83432616074",
     "74587887803",
     "87175659520"
-])('Deve testar se um cpf é válido', function (cpf: string) {
-    const isValid = validate(cpf)
-    expect(isValid).toBeTruthy();    
+])('Deve testar se um cpf é válido', function (value: string) {
+    const cpf = new Cpf(value);
+    expect(cpf.value).toBe(value);    
 });
 
 test.each([
@@ -14,7 +14,6 @@ test.each([
     "99999999999",
     "834326160",
     "",
-])('Deve testar se um cpf é inválido', function (cpf: string) {
-    const isValid = validate(cpf)
-    expect(isValid).toBeFalsy();    
+])('Deve testar se um cpf é inválido', function (value: string) {
+    expect(() => new Cpf(value)).toThrow(new Error("Invalid cpf"));
 });
