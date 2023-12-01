a45fbf6cb44280a0a83aca9d5ce8ffac1d031ba0
Ibrahim <ibrahim.borba@gmail.com>
2023-12-01 01:37:11 -0300

    refacotr: implement class instead of funtions for cpf validation

--- clean-code-clean-arch/backend/ride/src/cpf.ts
+++ clean-code-clean-arch/backend/ride/src/cpf.ts
@@ -1,35 +1,44 @@
-// @ts-nocheck
-function clean(cpf: string) {
-    return cpf.replace(/\D/g, "");
-}
-
-function isValidLength(cpf: string) {
-    return cpf.length === 11;
-}
-
-function hasAllDigitsEqual(cpf: string) {
-    const [firstDigit] = cpf;
-    return cpf.split("").every(digit => digit === firstDigit);
-}
-
-function extractDigitCpf(cpf: string) {
-    return cpf.substring(cpf.length-2, cpf.length);
-}
-
-function calculateDigit(cpf: string, factor: number) {
-    let total = 0;
-    for (const digit of cpf) {
-        if (factor > 1) total += parseInt(digit) * factor--;
+export default class Cpf{
+    value: string;
+    
+    constructor(value: string) {
+        if(!this.validate(value)) throw new Error("Invalid cpf");
+        this.value = value;
     }
-    const rest = total%11;
-    return (rest < 2) ? 0 : 11 - rest;
-}
 
-export function validate (cpf: string) {
-    cpf = clean(cpf);  
-    if (!isValidLength(cpf)) return false;
-    if (hasAllDigitsEqual(cpf)) return false;
-    const dg1 = calculateDigit(cpf, 10);                   
-    const dg2 = calculateDigit(cpf, 11);                   
-    return extractDigitCpf(cpf) == `${dg1}${dg2}`;
+    private validate(cpf: string) {
+        cpf = this.clean(cpf);  
+        if (!this.isValidLength(cpf)) return false;
+        if (this.hasAllDigitsEqual(cpf)) return false;
+        const dg1 = this.calculateDigit(cpf, 10);                   
+        const dg2 = this.calculateDigit(cpf, 11);                   
+        return this.extractDigitCpf(cpf) == `${dg1}${dg2}`;
+    }
+
+    private clean(cpf: string) {
+        return cpf.replace(/\D/g, "");
+    }
+    
+    private isValidLength(cpf: string) {
+        return cpf.length === 11;
+    }
+    
+    private hasAllDigitsEqual(cpf: string) {
+        const [firstDigit] = cpf;
+        return cpf.split("").every(digit => digit === firstDigit);
+    }
+    
+    private calculateDigit(cpf: string, factor: number) {
+        let total = 0;
+        for (const digit of cpf) {
+            if (factor > 1) total += parseInt(digit) * factor--;
+        }
+        const rest = total%11;
+        return (rest < 2) ? 0 : 11 - rest;
+    }
+
+    private extractDigitCpf(cpf: string) {
+        return cpf.substring(cpf.length-2, cpf.length);
+    }
+    
 }
