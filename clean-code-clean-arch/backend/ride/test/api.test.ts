4094cdb03a63a12c362ac0b94ef3386edfdc6e24
Ibrahim <ibrahim.borba@gmail.com>
2023-12-01 01:44:16 -0300

    test: add test for api route for passenger creation

--- clean-code-clean-arch/backend/ride/test/api.test.ts
+++ clean-code-clean-arch/backend/ride/test/api.test.ts
@@ -10,7 +10,7 @@ test('Deve fazer o calculo de preco de uma corrida durante o dia', async functio
             { distance: 10, date: '2021-03-01T10:00:00'}
         ]
     };
-    const response = await axios.post("http://localhost:3000/calculate_ride", input)
+    const response = await axios.post("http://localhost:3000/calculate_ride", input);
     const output = response.data;
     expect(output.price).toBe(21);
 });
@@ -22,7 +22,23 @@ test('Deve retornar um erro se a distancia for invalida', async function () {
             { distance: -10, date: '2021-03-01T10:00:00'}
         ]
     };
-    const response = await axios.post("http://localhost:3000/calculate_ride", input)
+    const response = await axios.post("http://localhost:3000/calculate_ride", input);
     expect(response.status).toBe(422);
     expect(response.data).toBe('Invalid distance');
+});
+
+test('Deve cadastrar o passageiro', async function () {
+    const input = {
+        name: "John Doe",
+        email: "john.doe@gmail.com",
+        document: "83432616074"
+    };
+    const responseCreatePassenger = await axios.post("http://localhost:3000/passengers", input);
+    const outuputCreatePassenger = responseCreatePassenger.data;
+    expect(outuputCreatePassenger.passengerId).toBeDefined();
+
+    const response = await axios.get(`http://localhost:3000/passengers/${outuputCreatePassenger.passengerId}`);
+    expect(response.data.name).toBe("John Doe");
+    expect(response.data.email).toBe("john.doe@gmail.com");
+    expect(response.data.document).toBe("83432616074");
 });
