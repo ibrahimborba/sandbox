1c699db8a9d3a24adce3f6dc0759b0b0cb4b5f95
Ibrahim <ibrahim.borba@gmail.com>
2023-12-08 00:23:00 -0300

    feat: add routes to create and get driver

--- clean-code-clean-arch/backend/ride/src/api.ts
+++ clean-code-clean-arch/backend/ride/src/api.ts
@@ -29,8 +29,8 @@ app.post("/passengers", async function(req, res) {
             "insert into clean_code_clean_arch.passenger (passenger_id, name, email, document) values ($1, $2, $3, $4)",
             [passengerId, req.body.name, req.body.email, req.body.document]
         );
-        await connection.$pool.end();
         res.json({passengerId});
+        await connection.$pool.end();
     } catch (error: any) {
         res.status(422).send(error.message);        
     }
@@ -44,6 +44,38 @@ app.get("/passengers/:passengerId", async function(req, res) {
     );
     await connection.$pool.end();
     res.json(passengerData);
+});
+
+app.post("/drivers", async function(req, res) {
+    try {
+        const connection = pgp()("postgres://postgres:123456@localhost:5432");
+        const driverId = crypto.randomUUID();
+        if (!validate(req.body.document)) throw new Error("Invalid cpf");
+        await connection.query(
+            "insert into clean_code_clean_arch.driver (driver_id, name, email, document, car_plate) values ($1, $2, $3, $4, $5)",
+            [driverId, req.body.name, req.body.email, req.body.document, req.body.carPlate]
+        );
+        res.json({driverId});
+        await connection.$pool.end();
+    } catch (error: any) {
+        res.status(422).send(error.message);        
+    }
+});
+
+app.get("/drivers/:driverId", async function(req, res) {
+    const connection = pgp()("postgres://postgres:123456@localhost:5432");
+    const [driverData] = await connection.query(
+        "select * from clean_code_clean_arch.driver where driver_id = $1",
+        [req.params.driverId]
+    );
+    await connection.$pool.end();
+    res.json({
+        driverId: driverData.driver_id,
+        name: driverData.name,
+        email: driverData.email,
+        document: driverData.document,
+        carPlate: driverData.car_plate
+    });
 });
 
 app.listen(3000);
