daa83f570976cd0030179f63ff7927b242fc0c6a
Ibrahim <ibrahim.borba@gmail.com>
2023-12-08 00:02:01 -0300

    fix: syntax error in create script

--- clean-code-clean-arch/backend/ride/create.sql
+++ clean-code-clean-arch/backend/ride/create.sql
@@ -1,9 +1,9 @@
 drop schema clean_code_clean_arch;
 create schema clean_code_clean_arch;
 
-create table clean_code_clean_arch.passenger {
+create table clean_code_clean_arch.passenger (
     passenger_id uuid primary key,
     name text,
     email text,
     document text
-}
+);
