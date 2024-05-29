5578d87642a4e01bd08fb5c803dfa70f485ca713
Ibrahim <ibrahim.borba@gmail.com>
2024-05-29 05:58:48 -0300

    feat: add Modal

--- adopt-me/src/Details.tsx
+++ adopt-me/src/Details.tsx
@@ -2,8 +2,11 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 import ErrorBoundary from "./ErrorBoundary";
+import { useState } from "react";
+import Modal from "./Modal";
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
   const results = useQuery({ queryKey: ["details", id], queryFn: fetchPet });
   if (results.isLoading) {
@@ -22,8 +25,19 @@ const Details = () => {
         <h3>
           {pet.animal} - {pet.breed} - {pet.city},{pet.state}
         </h3>
-        <button>Adopt {pet.name}</button>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
         <p>{pet.description}</p>
+        {showModal && (
+          <Modal>
+            <section>
+              <h1>Would you like to adopt {pet.name}?</h1>
+              <div className="buttons">
+                <button>Yes</button>
+                <button onClick={() => setShowModal(false)}>No</button>
+              </div>
+            </section>
+          </Modal>
+        )}
       </section>
     </>
   );
