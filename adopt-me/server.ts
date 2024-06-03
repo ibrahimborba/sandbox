60fa5cd323524026c9ee17c31f0ebfd603f9ab1e
Ibrahim <ibrahim.borba@gmail.com>
2024-06-03 05:41:58 -0300

    feat: add node streams

---
+++ adopt-me/server.ts
@@ -0,0 +1,38 @@
+import {fileURLToPath } from "url";
+import path from "path";
+import express from 'express';
+import fs from 'fs';
+import renderApp from "./dist/server/ServerApp.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const PORT = process.env.PORT || 3001;
+
+const html = fs.readFileSync(path.resolve(__dirname, './dist/client/index.html')).toString();
+
+const parts = html.split('not rendered');
+
+const app = express();
+app.use("/assets",
+    express.static(path.resolve(__dirname, './dist/client/assets'))
+);
+
+app.use((req, res) => {
+    res.write(parts[0]);
+    const stream = renderApp(req.url, {
+        onShellReady() {
+            stream.pipe(res)
+        },
+        onShellError(err) {
+            console.error(err);
+
+        },
+        onAllReeady() {
+            res.write(parts[1]);
+            res.end();
+        }
+    })
+});
+
+console.log(`listening on ht´´://localhost:${PORT}`);
+app.listen(PORT);
--- adopt-me/src/App.tsx
+++ adopt-me/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Suspense, lazy, useState } from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AdoptPetContext from "./AdoptPetContext";
 
@@ -25,32 +25,27 @@ const App = () => {
         background: "url(https://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
       }}
     >
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <AdoptPetContext.Provider value={adoptedPet}>
-            <Suspense
-              fallback={
-                <div className="loading-pane">
-                  <h2 className="loader">Loading</h2>
-                </div>
-              }
-            >
-              <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
-                <Link
-                  className="text-6xl text-white hover:text-gray-200"
-                  to="/"
-                >
-                  Adopt Me!
-                </Link>
-              </header>
-              <Routes>
-                <Route path="/details/:id" element={<Details />} />
-                <Route path="/" element={<SearchParams />} />
-              </Routes>
-            </Suspense>
-          </AdoptPetContext.Provider>
-        </QueryClientProvider>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AdoptPetContext.Provider value={adoptedPet}>
+          <Suspense
+            fallback={
+              <div className="loading-pane">
+                <h2 className="loader">Loading</h2>
+              </div>
+            }
+          >
+            <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
+              <Link className="text-6xl text-white hover:text-gray-200" to="/">
+                Adopt Me!
+              </Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </Suspense>
+        </AdoptPetContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 };
