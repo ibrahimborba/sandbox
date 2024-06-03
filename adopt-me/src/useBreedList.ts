c5713005b64be0d1a75c704381bc0f1c2a87452b
Ibrahim <ibrahim.borba@gmail.com>
2024-06-03 07:25:38 -0300

    refactor: type fetch calls and custom hook

---
+++ adopt-me/src/useBreedList.ts
@@ -0,0 +1,14 @@
+import { QueryStatus, useQuery } from "@tanstack/react-query";
+import fetchBreedList from "./fetchBreedList";
+import { Animal } from "./APIResponsesTypes";
+
+export default function useBreedList(animal: Animal) {
+  const results = useQuery({
+    queryKey: ["breeds", animal],
+    queryFn: fetchBreedList,
+  });
+  return [results?.data?.breeds ?? [], results.status] as [
+    string[],
+    QueryStatus,
+  ];
+}
