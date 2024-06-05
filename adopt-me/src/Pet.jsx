03e681763393c066fc398b2eafa6a258fb4d5461
Ibrahim <ibrahim.borba@gmail.com>
2024-06-05 05:28:45 -0300

    test: add test for Pet

--- adopt-me/src/Pet.jsx
+++ adopt-me/src/Pet.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 const Pet = (props) => {
   let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
-  if (props.images.length) {
+  if (props.images && props.images.length) {
     hero = props.images[0];
   }
   return (
     <Link to={`/details/${props.petID}`} className="relative block">
       <div className="image-container">
-        <img src={hero} alt={props.name} />
+        <img data-testid="thumbnail" src={hero} alt={props.name} />
       </div>
       <div className="absolute bottom-0 left-0 bg-gradient-to-tr from-white to-transparent">
         <h2>{props.name}</h2>
