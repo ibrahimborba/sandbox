52f03bdb68f3ad91ea6b3180bdbf043f3a5b84b2
Ibrahim <ibrahim.borba@gmail.com>
2024-06-05 06:01:02 -0300

    test: add test with fetch mock for useBreedList

--- adopt-me/src/__tests__/useBreedList.test.jsx
+++ adopt-me/src/__tests__/useBreedList.test.jsx
@@ -1,10 +1,9 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import useBreedList from "../useBreedList";
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { expect, test } from "vitest";
 import { Provider } from "react-redux";
 import store from "../store";
-
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,16 +16,14 @@ const queryClient = new QueryClient({
 });
 
 const ReduxProvider = ({ children, store }) => (
-    <Provider store={store}>{children}</Provider>
-  )
+  <Provider store={store}>{children}</Provider>
+);
 
 test("gives an empty list with no animal provided", async () => {
   const { result } = renderHook(() => useBreedList(""), {
     wrapper: ({ children }) => (
       <QueryClientProvider client={queryClient}>
-        <ReduxProvider store={store}>
-        {children}
-        </ReduxProvider>
+        <ReduxProvider store={store}>{children}</ReduxProvider>
       </QueryClientProvider>
     ),
   });
@@ -34,4 +31,26 @@ test("gives an empty list with no animal provided", async () => {
   const [breedList, status] = result.current;
   expect(breedList).toHaveLength(0);
   expect(status).toBe("loaded");
+});
+
+test("gives back breeds when given an animal", async () => {
+  const breeds = ["Havanese", "Poodle", "Labrador"];
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      animal: "dog",
+      breeds,
+    }),
+  );
+  const { result } = renderHook(() => useBreedList("dog"), {
+    wrapper: ({ children }) => (
+      <QueryClientProvider client={queryClient}>
+        <ReduxProvider store={store}>{children}</ReduxProvider>
+      </QueryClientProvider>
+    ),
+  });
+
+  await waitFor(() => expect(result.current[1]).toBe("loaded"));
+
+  const [breedList] = result.current;
+  expect(breedList).toEqual(breeds);
 });
