0c11bf6817b6319c3a6ec97348272c07a49be654
Ibrahim <ibrahim.borba@gmail.com>
2024-12-17 20:22:40 -0300

    feat: add text and style to home page

--- next-api-journal/app/page.tsx
+++ next-api-journal/app/page.tsx
@@ -1,3 +1,22 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <div>home</div>
+  return (
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="max-x-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">A Journal app.</h1>
+        <p className="text-2xl text-white/60 mb-4">
+          An app for tracking mood through out your life. All you have to do is
+          be honest.
+        </p>
+        <div>
+          <Link href="/journal">
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
+              get started
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
