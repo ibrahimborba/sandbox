293dfbf80565d0ebcd627e4ba69d16e04950487f
Ibrahim <ibrahim.borba@gmail.com>
2024-12-28 02:36:30 -0300

    feat: add fetch journal enttries

--- next-api-journal/app/(dashboard)/journal/page.tsx
+++ next-api-journal/app/(dashboard)/journal/page.tsx
@@ -1,4 +1,22 @@
-const JournalPage = () => {
+import { getUserByClerkID } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+const getEntries = async () => {
+  const user = await getUserByClerkID()
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  return entries
+}
+
+const JournalPage = async () => {
+  const entries = await getEntries()
   return (
     <div>
       <h1>Journal</h1>
