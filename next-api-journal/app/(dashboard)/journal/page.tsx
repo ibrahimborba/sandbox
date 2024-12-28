a409eff1ba5a9a874cb6622d703db2e8c6e61416
Ibrahim <ibrahim.borba@gmail.com>
2024-12-28 12:00:47 -0300

    feat: add entries

--- next-api-journal/app/(dashboard)/journal/page.tsx
+++ next-api-journal/app/(dashboard)/journal/page.tsx
@@ -1,3 +1,5 @@
+import EntryCard from '@/components/EntryCard'
+import NewEntryCard from '@/components/NewEntryCard'
 import { getUserByClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 
@@ -18,8 +20,14 @@ const getEntries = async () => {
 const JournalPage = async () => {
   const entries = await getEntries()
   return (
-    <div>
-      <h1>Journal</h1>
+    <div className="p-10 bg-zinc-400/10 h-full">
+      <h2 className="text-3xl mb-8">Journal</h2>
+      <div className="grid grid-cols-3 gap-4">
+        <NewEntryCard />
+        {entries.map((entry) => (
+          <EntryCard key={entry.id} entry={entry} />
+        ))}
+      </div>
     </div>
   )
 }
